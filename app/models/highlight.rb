774bf95bd1baada93d2eef0b410978786a71a16b
Irene Nguyen <irene.m.nguyen@gmail.com>
2014-03-07 13:16:56 -0800

    highlights now have friendly urls

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -1,7 +1,11 @@
 class Highlight < ActiveRecord::Base
+  extend FriendlyId
+  
   attr_accessible :title, :description, :user_id
   
   validates :title, :description, :user_id, :presence => true
   
   belongs_to :user
+   
+  friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140306012957) do
+ActiveRecord::Schema.define(:version => 20140307211136) do
 
   create_table "highlights", :force => true do |t|
     t.string   "title"
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(:version => 20140306012957) do
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "slug"
   end
 
+  add_index "highlights", ["slug"], :name => "index_highlights_on_slug", :unique => true
   add_index "highlights", ["user_id"], :name => "index_highlights_on_user_id"
 
   create_table "users", :force => true do |t|
